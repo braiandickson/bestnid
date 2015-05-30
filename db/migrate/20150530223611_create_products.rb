d1aa33799a1e75864d85838242adf2320402ccc2
braiandickson <braiandickson@gmail.com>
2015-05-30 19:38:29 -0300

    Crea el modelo de Productos

---
+++ db/migrate/20150530223611_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.date :due_date
+      t.string :state
+
+      t.timestamps null: false
+    end
+  end
+end
