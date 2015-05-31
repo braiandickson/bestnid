9cd8a09723c33511a2b3ea098742380a4032334b
braiandickson <braiandickson@gmail.com>
2015-05-31 00:27:54 -0300

    Acomoda las vistas del Usuario/Devise junto con algunos atributos del usuario como email.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,37 +1,51 @@
 class ProductsController < ApplicationController
-	before_action :find_product, only:[:show, :edit, :update, :destroy]
+	before_action :find_product, only: [:show, :edit, :update,:destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@products = Product.all.order("created_at DESC")
 	end
 
-	def show
-	end
-
 	def new
-		@product = Product.new
+		@product = current_user.products.build
 	end
 
 	def create
-		@product = Product.new(pin_params)
+		@product = current_user.products.build(product_params)
+
 		if @product.save
-			redirect_to @pin, notice: "Producto creado con exito."
+			redirect_to @product, notice: "Creado con exito!"
 		else
 			render 'new'
 		end
 	end
 
+	def show
 
+	end
+
+	def edit
+	end
+
+	def update
+		if @product.update(product_params)
+			redirect_to @product, notice: "Actualizado con exito!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+	end
 
 
 	private
 
 	def product_params
-		params.requiere(:product).permit(:name, :description, :due_date, :state)
+		params.require(:product).permit(:name, :description, :state, :due_date, :image)
 	end
 
 	def find_product
 		@product = Product.find(params[:id])
 	end
-
 end
