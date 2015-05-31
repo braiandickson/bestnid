5e3e2dee8a8236898f657a0c0de14eb4242880d6
braiandickson <braiandickson@gmail.com>
2015-05-31 13:16:21 -0300

    Agrega la migracion generada para due_date y period.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531034406) do
+ActiveRecord::Schema.define(version: 20150531160328) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.date     "due_date"
     t.string   "state"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
@@ -25,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150531034406) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "period"
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
