536f3f108aa117e7ca2add93174d7552895d47f0
braiandickson <braiandickson@gmail.com>
2015-05-30 21:46:36 -0300

    Agrega la entidad Producto con su controlador y vista correspondiente.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,37 @@
+class ProductsController < ApplicationController
+	before_action :find_product, only:[:show, :edit, :update, :destroy]
+
+	def index
+		@products = Product.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(pin_params)
+		if @product.save
+			redirect_to @pin, notice: "Producto creado con exito."
+		else
+			render 'new'
+		end
+	end
+
+
+
+
+	private
+
+	def product_params
+		params.requiere(:product).permit(:name, :description, :due_date, :state)
+	end
+
+	def find_product
+		@product = Product.find(params[:id])
+	end
+
+end
