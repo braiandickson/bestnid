5b9ac1917f2a7df2bcebb49e08b2e88488f117d2
Mariano Giagante <mariano.giagante@gmail.com>
2015-06-19 00:56:45 -0300

    agrega validación de longitud para tarjeta y email, así como sus mensajes de error

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :products
 
   email_regex = /\A.+@.+\..+$\z/
-  card_regex = /\d{16}/
+  card_regex = /\A\d+\z/
   name_regex = /\A\w+\z/
 
 	validates_presence_of :name, :email, :card, :password, :password_confirmation, :message => :required
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
 	validates_uniqueness_of :name, :email, :card, {:message => :taken} 
 
 	validates :email, format: {:with => email_regex, :message => :invalid}
+	validates :email, length: {:maximum => 25, :message => :too_long}
 	validates :card, format: {:with => card_regex, :message => :invalid}
+	validates :card, length: {:is => 16, :message => :invalid}
 
 end
