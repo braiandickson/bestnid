97f83d16ee06f79ccab7a6a2b3dcb377acc228a5
Mariano Giagante <mariano.giagante@gmail.com>
2015-06-21 22:30:36 -0300

    schema actualizado con la tabla inquiries y su relacion con productos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605151728) do
+ActiveRecord::Schema.define(version: 20150622010656) do
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string  "question"
+    t.string  "answer"
+    t.integer "product_id"
+  end
+
+  add_index "inquiries", ["product_id"], name: "index_inquiries_on_product_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
