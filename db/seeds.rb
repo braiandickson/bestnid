dfa1a619bc79b9626da07b10118571f9fff1e4e7
Mariano Giagante <mariano.giagante@gmail.com>
2015-06-24 23:46:30 -0300

    agrega seeds.rb que pone 2 usuarios de test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Create test user1
+User.create(
+	email: "test@test.com",
+	encrypted_password: "$2a$10$ryA06tbRXsFlssnQAL4ka.iZxiowcbFkSeSBNH8YezWwsdY/X3.tq",
+	name: "TestUser1",
+	card: "1234123412341234"
+)
+
+#Create test user2
+User.create(
+	email: "test2@test.com",
+	encrypted_password: "$2a$10$uOBJNpirhlkyBJZbRNHMnOGJmN0mmnCYIQcb87lzYNqHYf2rpvOXO",
+	name: "TestUser2",
+	card: "1111111111111111"
+)
