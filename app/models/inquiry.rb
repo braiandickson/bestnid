1951671ecfc1734e74eb2f32e346835743842a3e
Mariano Giagante <mariano.giagante@gmail.com>
2015-06-25 14:02:56 -0300

    agrega relacion belongs_to user

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,6 @@
 class Inquiry < ActiveRecord::Base
   belongs_to :product
+  belongs_to :user
 
   validates :question, presence: true, length: {:maximum => 250}
   validates :answer, length: {:maximum => 250}
