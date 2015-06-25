e0807b2dbaf45357751a3706a23959ee5dd211bc
Mariano Giagante <mariano.giagante@gmail.com>
2015-06-25 18:17:39 -0300

    implementa el responder pregunta y acomoda las vistas de preguntas

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -5,7 +5,6 @@ class InquiriesController < ApplicationController
 	end
 
 	def create
-		
 		@product = Product.find(params[:product_id])
 		@inquiry = @product.inquiries.create(params[:inquiry].permit(:question))
 		@inquiry.user_id = current_user.id if current_user
@@ -19,7 +18,26 @@ class InquiriesController < ApplicationController
 
 	end
 
+	def update
+		@product = Product.find(params[:product_id])
+		@inquiry = @product.inquiries.find_by(id: params[:id])
+		#byebug
+		if @inquiry.update(inquiry_params)
+      redirect_to @product, notice: "Pregunta respondida con éxito!"
+    else
+      render 'answer_questions'
+    end
+
+	end
+
 	def new
 	end
+
+	private
+
+	def inquiry_params
+		params.require(:inquiry).permit(:answer)
+	end
+
 
 end
