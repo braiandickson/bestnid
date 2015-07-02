0f9a59a0a00d8d5f120a2d8a52284425ec49ac52
Mariano Giagante <mariano.giagante@gmail.com>
2015-07-02 05:35:02 -0300

    agrega vista y controlador para el listado de ofertas del usuario actual

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -28,4 +28,8 @@ class BiddingsController < ApplicationController
 		flash[:notice] =  "Ud. ha elegido un ganador! Estamos procesando el cobro de su comisión. Gracias por operar con nosotros!"
 	end
 
+	def list_active_for_user
+		@biddings = Bidding.select { |b| b.user == current_user }
+	end
+
 end
