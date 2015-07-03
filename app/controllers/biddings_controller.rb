e7ce32a34d2554f0c722036aa0dddb69c711f544
Mariano Giagante <mariano.giagante@gmail.com>
2015-07-03 17:43:06 -0300

    soluciona problema de routing al ver subastas activas

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -38,7 +38,7 @@ class BiddingsController < ApplicationController
 		@bidding = Bidding.find(params[:id])
 		@bidding.update(:is_winner => true)
 		@bidding.product.update(:state => 'finished')
-		redirect_to product_path(params[:product_id])
+		redirect_to product_path(@bidding.product.id)
 		flash[:notice] =  "Ud. ha elegido un ganador! Estamos procesando el cobro de su comisión. Gracias por operar con nosotros!"
 	end
 
