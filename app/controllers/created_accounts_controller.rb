793b7994ce658ed4688da1eb19192770621ad8a9
Mariano Giagante <mariano.giagante@gmail.com>
2015-07-08 18:37:10 -0300

    incluye vista y controlador de created accounts con datepickers

--- app/controllers/created_accounts_controller.rb
+++ app/controllers/created_accounts_controller.rb
@@ -1,4 +1,15 @@
 class CreatedAccountsController < ApplicationController
+  
   def index
+
+  	#search_dates = {
+  	#	from_date: Date.today,
+  	#	to_date: Date.today
+  	#}
+  	from_date = params[:from_date] || Date.today
+  	to_date = params[:to_date] || Date.today
+  	byebug
+  	@created_accounts = CreatedAccount.where(registration_date: (from_date..to_date))
   end
+
 end
