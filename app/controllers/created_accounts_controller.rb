db7419ab063f38b236056ee1988687cbba5ee196
Mariano Giagante <mariano.giagante@gmail.com>
2015-07-16 03:08:10 -0300

    agrega validación de fechas cruzadas y mensaje de confirmación y bienvenida al reporte de usuarios registrados

--- app/controllers/created_accounts_controller.rb
+++ app/controllers/created_accounts_controller.rb
@@ -2,20 +2,30 @@ class CreatedAccountsController < ApplicationController
   
   def index
 
-  	params[:search_dates] ||= {
-  		date_from: Date.today,
-  		date_to: Date.today
-  		}
+#  	params[:search_dates] ||= {
+#  		date_from: Date.today,
+#  		date_to: Date.today
+#  		}
 
-  	date_from = params[:search_dates][:date_from] #|| Date.today
-  	date_to = params[:search_dates][:date_to] #|| Date.today
+  	if params[:search_dates]
 
-  	if date_from <= date_to
-  		@created_accounts = CreatedAccount.where(registration_date: (date_from..date_to))
-  	else
-  		@created_accounts = []
-			flash[:invalid] = "Por favor, ingrese un rango de fechas válido."
-  	end
+  		date_from = params[:search_dates][:date_from]
+  		date_to = params[:search_dates][:date_to]
+
+	  	if date_from <= date_to
+	  		@created_accounts = CreatedAccount.where(registration_date: (date_from..date_to))
+	  		flash.now[:notice] = "Se ha generado el reporte."
+	  	else
+	  		@created_accounts = []
+				flash.now[:invalid] = "Por favor, ingrese un rango de fechas válido."
+	  	end
+
+	  else
+	  	@created_accounts = []
+			flash.now[:notice] = "Bienvenido al reporte de usuarios registrados."
+	  end
+
+	  render 'index'
 
   end
 
